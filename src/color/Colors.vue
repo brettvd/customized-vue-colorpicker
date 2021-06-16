238ecce6aa2c34fcb99b69a2f3173c91966c2864
derpierre65 <git@derpierre65.de>
2021-06-16 11:19:50 +0200

    removed localStorage history

--- src/color/Colors.vue
+++ src/color/Colors.vue
@@ -1,40 +1,16 @@
 <template>
     <div>
         <ul class="colors">
-            <li
-                v-for="item in colorsDefault"
-                :key="item"
-                class="item"
-                @click="selectColor(item)"
-            >
-                <div
-                    :style="{ background: `url(${imgAlphaBase64})` }"
-                    class="alpha"
-                />
-                <div
-                    :style="{ background: item }"
-                    class="color"
-                />
+            <li v-for="item in colorsDefault" :key="item" class="item" @click="selectColor(item)">
+                <div :style="{ background: `url(${imgAlphaBase64})` }" class="alpha"/>
+                <div :style="{ background: item }" class="color"/>
             </li>
         </ul>
-        <ul
-            v-if="colorsHistory.length"
-            class="colors history"
-        >
-            <li
-                v-for="item in colorsHistory"
-                :key="item"
-                class="item"
-                @click="selectColor(item)"
-            >
-                <div
-                    :style="{ background: `url(${imgAlphaBase64})` }"
-                    class="alpha"
-                />
-                <div
-                    :style="{ background: item }"
-                    class="color"
-                />
+
+        <ul v-if="colorsHistory.length" class="colors history">
+            <li v-for="item in colorsHistory" :key="item" class="item" @click="selectColor(item)">
+                <div :style="{ background: `url(${imgAlphaBase64})` }" class="alpha"/>
+                <div :style="{ background: item }" class="color"/>
             </li>
         </ul>
     </div>
@@ -56,13 +32,17 @@ export default {
         colorsHistoryKey: {
             type: String,
             default: ''
+        },
+        history: {
+          type: Array,
+          default: () => [],
         }
     },
     data() {
         return {
             imgAlphaBase64: '',
-            colorsHistory: JSON.parse(localStorage.getItem(this.colorsHistoryKey)) || []
-        }
+            colorsHistory: this.history,
+        };
     },
     created() {
         this.imgAlphaBase64 = this.createAlphaSquare(4).toDataURL()
@@ -78,17 +58,20 @@ export default {
             if (!color) {
                 return
             }
+
             const colors = this.colorsHistory
             const index = colors.indexOf(color)
+
             if (index >= 0) {
                 colors.splice(index, 1)
             }
+
             if (colors.length >= 8) {
                 colors.length = 7
             }
+
             colors.unshift(color)
             this.colorsHistory = colors
-            localStorage.setItem(this.colorsHistoryKey, JSON.stringify(colors))
         }
     }
 }
